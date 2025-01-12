798df3ac2cbbb50e9eb1499812242b35d54f7cfa
White-Tiger-PX <191647395+White-Tiger-PX@users.noreply.github.com>
2025-01-12 13:02:33 +0300

    Рефакторинг логики обработки дат

--- auto_moving_files.py
+++ auto_moving_files.py
@@ -3,8 +3,8 @@ import time
 import json
 import logging
 
-from shutil import copy2, move
 from datetime import datetime
+from shutil import copy2, move
 
 
 def save_directory(directories_data, directory_path):
@@ -47,59 +47,24 @@ def checking_the_condition_for_action(program_start_time, path_settings, file_na
             directories_data[directory_path]['files'] = new_files_info
 
 
-def save_json(file_path, data):
-    try:
-        file_path = os.path.normpath(file_path)
+def append_time_str(string_datetime, destination_directory_path, period):
+    period_formats = {
+        'Y': '%Y',
+        'm': '%m',
+        'd': '%d',
+        'md': '%m-%d',
+        'Yd': '%Y-%d',
+        'Ym': '%Y-%m',
+        'Ymd': '%Y-%m-%d'
+    }
 
-        with open(file_path, 'w', encoding='utf-8') as file:
-            json.dump(data, file, ensure_ascii=False, indent=4)
-    except Exception as err:
-        logger.error("Ошибка при сохранении файла %s: %s", file_path, err)
+    name = string_datetime.strftime(period_formats.get(period, ''))
 
-
-def update_files_info(program_start_time, directory_path, file_names, archive_directory_data, directory_data):
-    files_data = {}
-
-    for file_name in file_names:
-        file_path = os.path.join(directory_path, file_name)
-
-        try:
-            archive_file_data = archive_directory_data.pop(file_path)
-            file_first_seen_time = archive_file_data['file_first_seen_time']
-        except Exception:
-            file_first_seen_time = program_start_time
-
-        try:
-            file_modified_time = os.path.getmtime(file_path)
-        except Exception:
-            file_modified_time = program_start_time
-
-        files_data[file_path] = {
-            "name": file_name,
-            "file_modified_time": file_modified_time,
-            "file_first_seen_time": file_first_seen_time
-        }
-
-    directory_data['files'] = files_data
+    return os.path.join(destination_directory_path, name)
 
 
-def sorting_with_date(string_datetime, setup, destination_directory_path):
-    def append_time_str(string_datetime, destination_directory_path, period):
-        if period == 'Y':
-            name = str(string_datetime.strftime('%Y'))
-        elif period == 'm':
-            name = str(string_datetime.strftime('%m'))
-        elif period == 'd':
-            name = str(string_datetime.strftime('%d'))
-        elif period == 'Ym':
-            name = str(string_datetime.strftime('%Y-%m'))
-        elif period == 'Ymd':
-            name = str(string_datetime.strftime('%Y-%m-%d'))
-        else:
-            name = None
-
-        destination_directory_path = os.path.join(destination_directory_path, name)
-        return destination_directory_path
+def sorting_with_date(program_start_time, setup, destination_directory_path):
+    string_datetime = datetime.fromtimestamp(program_start_time)
 
     if setup['sort_by_year']:
         destination_directory_path = append_time_str(string_datetime, destination_directory_path, 'Y')
@@ -128,7 +93,6 @@ def copy_files(program_start_time, path_settings, directories_data):
     params = path_settings['sorting_with_date']
     sort_by_date = params['sort_by_days'] or params['sort_by_month'] or params['sort_by_year']
     len_input_path = len(path_settings['input']) + 1
-    string_datetime = datetime.fromtimestamp(program_start_time)
 
     for directory_path, directory_info in directories_data.items():
         for file_path, move_file in directory_info['files'].items():
@@ -136,18 +100,18 @@ def copy_files(program_start_time, path_settings, directories_data):
                 if save_folders:
                     if sort_by_date:
                         if params['in_the_root_folder']:
-                            destination_directory_path = sorting_with_date(string_datetime, params, path_settings['output'])
+                            destination_directory_path = sorting_with_date(program_start_time, params, path_settings['output'])
                             destination_directory_path = os.path.join(destination_directory_path, directory_path[len_input_path:])
                         else:
                             destination_directory_path = os.path.join(path_settings['output'], directory_path[len_input_path:])
-                            destination_directory_path = sorting_with_date(string_datetime, params, destination_directory_path)
+                            destination_directory_path = sorting_with_date(program_start_time, params, destination_directory_path)
                     else:
                         destination_directory_path = os.path.join(path_settings['output'], directory_path[len_input_path:])
                 else:
                     destination_directory_path = path_settings['output']
 
                     if sort_by_date:
-                        destination_directory_path = sorting_with_date(string_datetime, params, destination_directory_path)
+                        destination_directory_path = sorting_with_date(program_start_time, params, destination_directory_path)
 
                 destination_directory_path = os.path.join(destination_directory_path, os.path.basename(file_path))
 
@@ -164,63 +128,104 @@ def copy_files(program_start_time, path_settings, directories_data):
                         if not os.path.isfile(os.path.dirname(destination_directory_path)):
                             os.makedirs(os.path.dirname(destination_directory_path), exist_ok=True)
                         else:
-                            logging.error("Невозможно создать директорию %s, так как файл с таким же именем уже существует.", os.path.dirname(destination_directory_path))
+                            logger.error("Невозможно создать директорию %s, так как файл с таким же именем уже существует.", os.path.dirname(destination_directory_path))
                             continue
 
                         rel_dir_path = os.path.relpath(directory_path, path_settings['input'])
                         file_path_with_last_folder = os.path.join(rel_dir_path, os.path.basename(file_path))
 
                         copy2(file_path, destination_directory_path)
-                        logging.info("Скопирован файл %s", file_path_with_last_folder)
+                        logger.info("Скопирован файл %s", file_path_with_last_folder)
                     except Exception as error:
-                        logging.error("Ошибка при копировании файла %s в %s: %s", file_path_with_last_folder, os.path.dirname(destination_directory_path), error)
+                        logger.error("Ошибка при копировании файла %s в %s: %s", file_path_with_last_folder, os.path.dirname(destination_directory_path), error)
 
 
 def moving_files(program_start_time, path_settings, directories_data):
     save_folders = path_settings['save_folders']
-    params = path_settings['sorting_with_date']
-    sort_by_date = params['sort_by_days'] or params['sort_by_month'] or params['sort_by_year']
+    sorting_with_date_options = path_settings['sorting_with_date']
+    sort_by_date = any(sorting_with_date_options[key] for key in ['sort_by_days', 'sort_by_month', 'sort_by_year'])
     len_input_path = len(path_settings['input']) + 1
-    string_datetime = datetime.fromtimestamp(program_start_time)
 
     for directory_path, directory_info in directories_data.items():
         for file_path, move_file in directory_info['files'].items():
-            if move_file:
-                if save_folders:
-                    if sort_by_date:
-                        if params['in_the_root_folder']:
-                            destination_directory_path = sorting_with_date(string_datetime, params, path_settings['output'])
-                            destination_directory_path = os.path.join(destination_directory_path, directory_path[len_input_path:])
-                        else:
-                            destination_directory_path = os.path.join(path_settings['output'], directory_path[len_input_path:])
-                            destination_directory_path = sorting_with_date(string_datetime, params, destination_directory_path)
+            if not move_file:
+                continue
+
+            if save_folders:
+                if sort_by_date:
+                    if sorting_with_date_options['in_the_root_folder']:
+                        destination_directory_path = sorting_with_date(program_start_time, sorting_with_date_options, path_settings['output'])
+                        destination_directory_path = os.path.join(destination_directory_path, directory_path[len_input_path:])
                     else:
                         destination_directory_path = os.path.join(path_settings['output'], directory_path[len_input_path:])
+                        destination_directory_path = sorting_with_date(program_start_time, sorting_with_date_options, destination_directory_path)
                 else:
-                    destination_directory_path = path_settings['output']
+                    destination_directory_path = os.path.join(path_settings['output'], directory_path[len_input_path:])
+            else:
+                destination_directory_path = path_settings['output']
 
-                    if sort_by_date:
-                        destination_directory_path = sorting_with_date(string_datetime, params, destination_directory_path)
+                if sort_by_date:
+                    destination_directory_path = sorting_with_date(program_start_time, sorting_with_date_options, destination_directory_path)
 
-                destination_directory_path = os.path.join(destination_directory_path, os.path.basename(file_path))
+            destination_directory_path = os.path.join(destination_directory_path, os.path.basename(file_path))
 
-                par1 = file_path != destination_directory_path
-                par2 = not os.path.exists(destination_directory_path) or path_settings['overwrite_files']
+            par1 = file_path != destination_directory_path
+            par2 = not os.path.exists(destination_directory_path) or path_settings['overwrite_files']
 
-                if par1 and par2:
-                    try:
-                        if not os.path.isfile(os.path.dirname(destination_directory_path)):
-                            os.makedirs(os.path.dirname(destination_directory_path), exist_ok=True)
-                        else:
-                            logging.error("Невозможно создать директорию %s, так как файл с таким же именем уже существует.", os.path.dirname(destination_directory_path))
-                            continue
+            if par1 and par2:
+                try:
+                    if not os.path.isfile(os.path.dirname(destination_directory_path)):
+                        os.makedirs(os.path.dirname(destination_directory_path), exist_ok=True)
+                    else:
+                        logger.error("Невозможно создать директорию %s, так как файл с таким же именем уже существует.", os.path.dirname(destination_directory_path))
+                        continue
 
-                        file_path_with_last_folder = os.path.join(os.path.relpath(directory_path, path_settings['input']), os.path.basename(file_path))
+                    move(file_path, destination_directory_path)
+                    logger.info("Перемещен файл %s", file_path)
+                except Exception as error:
+                    logger.error("Ошибка при перемещении файла %s в %s: %s", file_path, os.path.dirname(destination_directory_path), error)
 
-                        move(file_path, destination_directory_path)
-                        logging.info("Перемещен файл %s", file_path_with_last_folder)
-                    except Exception as error:
-                        logging.error("Ошибка при перемещении файла %s в %s: %s", file_path_with_last_folder, os.path.dirname(destination_directory_path), error)
+
+def save_json(file_path, data):
+    try:
+        file_path = os.path.normpath(file_path)
+
+        with open(file_path, 'w', encoding='utf-8') as file:
+            json.dump(data, file, ensure_ascii=False, indent=4)
+    except Exception as err:
+        logger.error("Ошибка при сохранении файла %s: %s", file_path, err)
+
+
+def update_files_info(program_start_time, directory_path, file_names, archive_directory_data, directory_data):
+    files_data = {}
+
+    for file_name in file_names:
+        file_path = os.path.join(directory_path, file_name)
+
+        try:
+            archive_file_data = archive_directory_data.get(file_path, None)
+
+            if archive_file_data:
+                file_first_seen_time = archive_file_data['file_first_seen_time']
+            else:
+                file_first_seen_time = program_start_time
+        except Exception:
+            file_first_seen_time = program_start_time
+            logger.error("Ошибка получения архивных данных первого обнаружения файла: %s", file_path)
+
+        try:
+            file_modified_time = os.path.getmtime(file_path)
+        except Exception:
+            file_modified_time = program_start_time
+            logger.error("Ошибка получения времени последнего изменения файла: %s", file_path)
+
+        files_data[file_path] = {
+            "name": file_name,
+            "file_modified_time": file_modified_time,
+            "file_first_seen_time": file_first_seen_time
+        }
+
+    directory_data['files'] = files_data
 
 
 def directory_walk(program_start_time, root_directory_path, archive_data, directories_data):
@@ -252,8 +257,8 @@ def update_dir_info(program_start_time, directory_with_scanned_directories, dire
     if not os.path.exists(directory_path):
         return archive_data
 
-    correct_directory_path = directory_path.replace('/', '_').replace(':', '')
-    path = f"{directory_with_scanned_directories}/{correct_directory_path}.json"
+    archive_file_name = directory_path.replace('/', '_').replace(':', '')
+    path = f"{directory_with_scanned_directories}/{archive_file_name}.json"
 
     archive_data = load_json(path, default_type={})
 
